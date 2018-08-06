1a1f92c7729fec1c062a43b321315667d52d0342
Fernando Simoes <fernando@nationphone.com>
2018-08-06 00:31:57 -0300

    Converting user.js to class user following concepts of class like react js

--- src/js/formfunctions.js
+++ src/js/formfunctions.js
@@ -2,20 +2,25 @@
 export const submitEventListener = (datatable, eventsFunction) => {
     document.querySelector('#submitButton').addEventListener('click', () => {
         const form = document.forms[0];
-        const username = form.querySelector('input[name="username"]') != null ? form.querySelector('input[name="username"]').value : '';
+        const username = (form.querySelector('input[name="username"]') && form.querySelector('input[name="username"]').value.length > 0 ) ? form.querySelector('input[name="username"]').value : formValidate(form.querySelector('input[name="username"]'), 'email');
         const name = form.querySelector('input[name="name"]') != null ? form.querySelector('input[name="name"]').value : '';
         const email = form.querySelector('input[name="email"]') != null ? form.querySelector('input[name="email"]').value : '';
         const city = form.querySelector('input[name="city"]') != null ? form.querySelector('input[name="city"]').value : '';
         const rideingroup = form.querySelector('input[name="ridegroup"]:checked') != null ? form.querySelector('input[name="ridegroup"]:checked').value : '';
         const weekdayelements = form.querySelector('input[name="weekday"]:checked') != null ? Array.from(form.querySelectorAll('input[name="weekday"]:checked')) : '';
 
-        let weekdays = ""
-        weekdayelements.forEach((element, key) => {
-            weekdays.length == 0 ? weekdays = element.value : weekdays = `${weekdays} ${element.value}`;
-        });
+        formValidate('', true);
+        let weekdays = '';
+        if (weekdayelements.length) {
+            weekdayelements.forEach((element, key) => {
+                weekdays.length == 0 ? weekdays = element.value : weekdays = `${weekdays} ${element.value}`;
+            });
+        } else {
+            return false;
+        }
 
 
-        console.log(username, name, email, city, rideingroup, weekdays);
+        // console.log(username, name, email, city, rideingroup, weekdays);
 
         const newLine = [
             username,
@@ -27,7 +32,7 @@ export const submitEventListener = (datatable, eventsFunction) => {
             rideingroup,
             weekdays,
             '<div class="posts">0</div>',
-            '<div class="albums">0</div>',,
+            '<div class="albums">0</div>',
             '0',
             `<div class="trash" data-userid='' data-tableindex='${document.querySelectorAll('.dataTable-table tbody tr').length}'>   
             <a>
@@ -35,7 +40,22 @@ export const submitEventListener = (datatable, eventsFunction) => {
             </a>
         </div>`
         ]
+        console.log('newLine', newLine);
         datatable.rows().add(newLine);
         eventsFunction();
     })
+}
+
+
+
+const formValidate = (element, key) => {
+
+    switch (key) {
+        case 'email':
+             return 'email'
+            break;
+
+        default:
+            break;
+    }
 }
--- src/js/users.js
+++ src/js/users.js
@@ -1,4 +1,4 @@
-
+import ajaxrequests from './ajaxRequests';
 import DataTable from 'vanilla-datatables';
 import axios from 'axios';
 import alertify from 'alertifyjs';
@@ -8,247 +8,194 @@ let table = new DataTable("#usertable",{
     perPageSelect: false,
     perPage: 50,
 });
-submitEventListener(table, events)
-
-
-let photos, posts, albums, lielements;
-let groupedItems = []
-
-
-
-// random generate day of the week
-const dayoftheweek = () => {
-    const numberDays = Math.floor(Math.random() * 7);
-    if (numberDays == 6) {
-        return 'Every Day'
-    } else if (numberDays == 5) {
-        return 'Weekend';
-    } else if (numberDays == 0) {
-        return 'Mon'
-    } else {
-        let days = '';
-        for (let i = 0; i < numberDays; i++) {
-            let day = Math.floor(Math.random() * 5);
-            if (day == 0 && days.indexOf('Mon') == -1) {
-                days = days.length > 0 ? `${days} Mon` : 'Mon'
-             }
-            if (day == 1 && days.indexOf('Tue')==-1) {
-                days = days.length > 0 ? `${days} Tue` : 'Tue'
-             }
-            if (day == 2 && days.indexOf('Wed') == -1) { 
-                days = days.length > 0 ? `${days} Wed` : 'Wed'
-             }
-            if (day == 3 && days.indexOf('Thu') == -1) { 
-                days = days.length > 0 ? `${days} Thu` : 'Thu'
-            }
-            if (day == 4 && days.indexOf('Fri') == -1) {
-                days = days.length > 0 ? `${days} Fri` : 'Fri'
-            }
-        }
-        return days;
-    }
-}
-// random generate ride in froup
-const rideingroup = () => {
-    const numberDays = Math.floor(Math.random() * 3);
-    if (numberDays == 2) {
-        return 'Always'
-    } else if (numberDays == 1) {
-        return 'Sometimes';
-    } else if (numberDays == 0) {
-        return 'Never'
-    }
-}
-
-const getPosts = () => {
-    return axios.get('https://jsonplaceholder.typicode.com/posts')
-}
-
-const getAlbums = () => {
-    return axios.get('https://jsonplaceholder.typicode.com/albums')
-        
-}
-
-const getPhotos = () => {
-    return axios.get('https://jsonplaceholder.typicode.com/photos')
-        
-}
-
-
-const groupItemsByUser = (user) => {
-    // console.log('user', user);
-    let postByUser = [];
-    let albumByUser = [];
-    let photoByUser = [];
-    posts.forEach((post, key) => {
-        if (post.userId == user.id) {
-            postByUser.push(post);
-        }
-    })
-    albums.forEach((album, key) => {
-        if(album.userId == user.id) {
-            albumByUser.push(album);
-            photos.forEach(photo => {
-                if (album.id == photo.albumId) {
-                    photoByUser.push(photo);
-                }
-            })
-        }
-    })
-    groupedItems.push({
-        userId: user.id,
-        userinfo: user,
-        posts: postByUser,
-        albums: albumByUser,
-        photos: photoByUser
-    })
-    // console.log('groupedItems', groupedItems);
-
-}
-
-// get users from json place holder and include in dom datatable;
-const getUsers = () => {
-    return new Promise((resolve, reject) => {
-
-
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(function (response) {
-            console.log('response', response.data.length);
-            response.data.forEach(function (user, key) {
-                // console.log('key', key);
-                // axios.get(https://jsonplaceholder.typicode.com/albums)
-                groupItemsByUser(user);
-            });
-
-            resolve(groupedItems)
-
-            // return true;
-            
-        })
-    })
-
-}
-// save items in a first call of this single page app
-getPosts().then(function (response) {
-    posts = response.data;
-    getAlbums().then(function (response) {
-        albums = response.data;
-    })
-    getPhotos().then(function (response) {
-        photos = response.data;
-        // console.log('teste', photos, posts, albums);
-        getUsers().then(response => {
-            generateTable();
+class user {
+    constructor() {
+        this.photos = 0
+        this.posts = 0
+        this.albums = 0
+        this.lielements = 0;
+        this.table = new DataTable("#usertable", {
+            perPageSelect: false,
+            perPage: 50,
         });
-    });
-})
+
+        this.groupedItems = []
+        this.removeItem = this.removeItem.bind(this);
+        this.events = this.events.bind(this);
+
+        const _self = this;
+        submitEventListener(table, _self.events)
+        ajaxrequests.getPosts().then(function (response) {
+            _self.posts = response.data;
+            ajaxrequests.getAlbums().then(function (response) {
+                _self.albums = response.data;
+                ajaxrequests.getPhotos().then(function (response) {
+                    _self.photos = response.data;
+                    ajaxrequests.getUsers().then(function (response) {
+                        response.data.forEach(function (user, key) {
+                            // console.log('key', key);
+                            // axios.get(https://jsonplaceholder.typicode.com/albums)
+                            _self.groupItemsByUser(user);
+                        });
+                        // resolve(groupedItems)
+                        _self.generateTable();
+                        // return true;
 
 
-const generateTable = () => {
+                    })
+                });
+            })
+        })
+    }
 
-    let datatable = table;
-    let users = [];
 
-    groupedItems.forEach((user, key) => {
-        users.push([
-            user.userinfo.username,
-            user.userinfo.name,
-            `<a href="mailto:${user.userinfo.email}">
+    dayoftheweek() {
+        const numberDays = Math.floor(Math.random() * 7);
+        if (numberDays == 6) {
+            return 'Every Day'
+        } else if (numberDays == 5) {
+            return 'Weekend';
+        } else if (numberDays == 0) {
+            return 'Mon'
+        } else {
+            let days = '';
+            for (let i = 0; i < numberDays; i++) {
+                let day = Math.floor(Math.random() * 5);
+                if (day == 0 && days.indexOf('Mon') == -1) {
+                    days = days.length > 0 ? `${days} Mon` : 'Mon'
+                }
+                if (day == 1 && days.indexOf('Tue') == -1) {
+                    days = days.length > 0 ? `${days} Tue` : 'Tue'
+                }
+                if (day == 2 && days.indexOf('Wed') == -1) {
+                    days = days.length > 0 ? `${days} Wed` : 'Wed'
+                }
+                if (day == 3 && days.indexOf('Thu') == -1) {
+                    days = days.length > 0 ? `${days} Thu` : 'Thu'
+                }
+                if (day == 4 && days.indexOf('Fri') == -1) {
+                    days = days.length > 0 ? `${days} Fri` : 'Fri'
+                }
+            }
+            return days;
+        }
+    }
+
+    rideingroup() {
+        const numberDays = Math.floor(Math.random() * 3);
+        if (numberDays == 2) {
+            return 'Always'
+        } else if (numberDays == 1) {
+            return 'Sometimes';
+        } else if (numberDays == 0) {
+            return 'Never'
+        }
+    }
+
+    groupItemsByUser(user) {
+        // console.log('user', user);
+        const _self = this;
+        let postByUser = [];
+        let albumByUser = [];
+        let photoByUser = [];
+        this.posts.forEach((post, key) => {
+            if (post.userId == user.id) {
+                postByUser.push(post);
+            }
+        })
+        this.albums.forEach((album, key) => {
+            if (album.userId == user.id) {
+                albumByUser.push(album);
+                _self.photos.forEach(photo => {
+                    if (album.id == photo.albumId) {
+                        photoByUser.push(photo);
+                    }
+                })
+            }
+        })
+        this.groupedItems.push({
+            userId: user.id,
+            userinfo: user,
+            posts: postByUser,
+            albums: albumByUser,
+            photos: photoByUser
+        })
+        // console.log('groupedItems', groupedItems);
+
+    }
+
+    generateTable() {
+        const _self = this;
+        let users = [];
+
+        this.groupedItems.forEach((user, key) => {
+            users.push([
+                user.userinfo.username,
+                user.userinfo.name,
+                `<a href="mailto:${user.userinfo.email}">
                 ${user.userinfo.email}
             </a>`,
-            `<a target="_blank" href="https://www.google.com/maps/search/?api=1&query=${user.userinfo.address.geo.lat},${user.userinfo.address.geo.lng}">
+                `<a target="_blank" href="https://www.google.com/maps/search/?api=1&query=${user.userinfo.address.geo.lat},${user.userinfo.address.geo.lng}">
                 ${user.userinfo.address.city}
             </a>`,
-            rideingroup(),
-            dayoftheweek(),
-            `<div class="posts">${user.posts.length}</div>`,
-            `<div class="albums">${user.albums.length}</div>`,
-            user.photos.length,
-            `<div class="trash" data-userid='${user.userId}' data-tableindex='${key}'>   
+                _self.rideingroup(),
+                _self.dayoftheweek(),
+                user.posts.length,
+                user.albums.length,
+                user.photos.length,
+                `<div class="trash" data-userid='${user.userId}' data-tableindex='${key}'>   
                 <a>
                     <i class="fas fa-trash-alt"></i>
                 </a>
             </div>`
-        ])
+            ])
 
-    })
-    datatable.rows().add(users)
-    events();
-}
-const events = () => {
-
-    document.querySelectorAll('.trash').forEach(function (e) {
-        e.addEventListener('click', (e) => {
-            // console.log('e', e);
-            
-            const domElement = e.target.parentElement.parentElement
-            const userid = domElement.dataset;
-
-            alertify.confirm(
-                'Delete User', 'Do you really wants delete user?', 
-                function () { 
-                    removeItem(userid, domElement)
-                }
-                , function () { 
-
-                }
-            );
         })
-    })
+        table.rows().add(users)
+        this.events();
+    }
+    events() {
+        const _self = this;
 
-   
+        document.querySelectorAll('.trash').forEach(function (e) {
+            e.addEventListener('click', (e) => {
+                // console.log('e', e);
 
-    
+                const domElement = e.target.parentElement.parentElement
+                const userid = domElement.dataset;
 
-}
+                alertify.confirm(
+                    'Delete User', 'Do you really wants delete user?',
+                    function () {
+                        _self.removeItem(userid, domElement)
+                    }
+                    , function () {
 
-const hideelements = () => {
-    
-    lielements = document.querySelectorAll('.breadcrumb--list li')
-    const elementslenght = lielements.length - 1;
-    lielements.forEach((e, k) => {
-        if (k >= 2 && k < elementslenght) {
-            if (!e.classList.contains('ellipseshide')) {
-                e.classList.add('hide');
+                    }
+                );
+            })
+        })
+    }
+
+    removeItem(userId, element) {
+        console.log('element', element)
+        const dataindex = element.dataset;
+        console.log('dataindex', dataindex)
+        /** this block update interface and a request for delete is called */
+        table.rows().remove(dataindex.tableindex);
+        element.remove();
+        alertify.success('Ok')
+        //end block
+        this.events()
+        /* in this point i was submit my request to delete to api and in success or error update interface.
+        axios.delete(`https://jsonplaceholder.typicode.com/users/${dataindex.userid}`)
+        .then(function (response) {
+            console.log(response);
+            if(response.status == 200 ) {
             }
-        }
-        if (k == elementslenght) {
-            console.log('e', e);
-            document.querySelector('.ellipseshide').classList.remove('ellipseshide')
-            // document.querySelector('.ellipseshide').classList.add('ellipsesShow')
-            // <li>
-            //     <span>PAGE NAME 1</span>
-            //     <div class="arrow">
-            //         <i class="fas fa-chevron-right"></i>
-            //     </div>
-            // </li>
-        }
-    })
+        })
+        */
+    }
 }
 
-const showElements = () => {
-
-}
-hideelements()
-const removeItem = (userId, element) => {
-    console.log('element', element)
-    const dataindex = element.dataset;
-    console.log('dataindex', dataindex)
-    /** this block update interface and a request for delete is called */
-    table.rows().remove(dataindex.tableindex);
-    element.remove();
-    alertify.success('Ok') 
-    //end block
-    events()
-    /* in this point i was submit my request to delete to api and in success or error update interface.
-    axios.delete(`https://jsonplaceholder.typicode.com/users/${dataindex.userid}`)
-    .then(function (response) {
-        console.log(response);
-        if(response.status == 200 ) {
-        }
-    })
-    */
-}
-
-
-
+new user();
